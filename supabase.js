3a5baa3a680eacae63e8a1b43fd679ec736a3d9c
donmoyero <63630549+donmoyero@users.noreply.github.com>
2026-03-08 21:55:55 -0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://zoenpjjhzdzcodoqsxap.supabase.co";
-
-const SUPABASE_ANON_KEY = "sb_publishable_DWdVt8DhlzgritPEsDCEow_N-Wy-V_I";
-
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
